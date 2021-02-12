99cec352c925cdb2adf6d5257814c4b78ebf4419
Daniel Hoffman <dhoffman@longform.tech>
2021-02-11 18:03:51 -0600

    update iced from 0.1 to 0.2, move periodic subscription over to the new format

--- src/lib.rs
+++ src/lib.rs
@@ -353,36 +353,7 @@ impl VideoPlayer {
 
 // until iced 0.2 is released, which has this built-in
 mod time {
-    use iced::futures;
-
     pub fn every(duration: std::time::Duration) -> iced::Subscription<std::time::Instant> {
-        iced::Subscription::from_recipe(Every(duration))
-    }
-
-    struct Every(std::time::Duration);
-
-    impl<H, I> iced_native::subscription::Recipe<H, I> for Every
-    where
-        H: std::hash::Hasher,
-    {
-        type Output = std::time::Instant;
-
-        fn hash(&self, state: &mut H) {
-            use std::hash::Hash;
-
-            std::any::TypeId::of::<Self>().hash(state);
-            self.0.hash(state);
-        }
-
-        fn stream(
-            self: Box<Self>,
-            _input: futures::stream::BoxStream<'static, I>,
-        ) -> futures::stream::BoxStream<'static, Self::Output> {
-            use futures::stream::StreamExt;
-
-            tokio::time::interval(self.0)
-                .map(|_| std::time::Instant::now())
-                .boxed()
-        }
+	iced::time::every(duration)
     }
 }
